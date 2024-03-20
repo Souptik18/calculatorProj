1445a469774bdb4ecc6eff2baa9146a4d22b7f2b
Souptik18 <souptiksarkar2017@gmail.com>
2024-03-20 21:15:14 +0530

    first Commit without keypress

---
+++ script.js
@@ -0,0 +1,8 @@
+function hovered(event) {
+  let hoverBtn = document.querySelector("button");
+  hoverBtn.style.transform = "scale(1.10)";
+}
+function resetBtn(event) {
+  let hoverBtn = document.querySelector("button");
+  hoverBtn.style.transform = "scale(1)";
+}
